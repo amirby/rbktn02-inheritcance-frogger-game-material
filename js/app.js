9fc4be39ea57608e69a527245588fb6dad58bf2a
Amir Ben Youssef <amirbenyoussef3@gmail.com>
2020-03-25 14:10:10 +0100

    third trial

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var Enemy = function(x,y) {
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.max = 100
-    this.speed = (Math.random()* this.max)+10
+ 
+    this.speed = Math.random()* (200 - 20)+20
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -17,10 +17,26 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x+=this.speed*dt
+    if ((player.y-40<this.y) && (this.y<player.y+40) && (player.x-40<this.x) && (this.x<player.x+40)  ){
+        player.reset()
+        this.speed = Math.random()* (200 - 20)+20
+        player.score = 0
+        $('.badge').text(player.score)
+        if(player.life >= 2){
+            $("#hearts img:last-child").remove()
+            player.life--
+        }else {
+            $("#GameOver").modal("show");
+            player.life--
+        }
+
+    }
     if(this.x > 550) {
         this.x = -200;
-    }
+    } 
+    else  {  
+        this.x+=this.speed*dt
+    }  
 };
 
 // Draw the enemy on the screen, required method for game
@@ -34,6 +50,7 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.score = 0
+    this.life = 3
     this.x = 200;
     this.y = 370;
     this.sprite = 'images/char-boy.png';
@@ -44,6 +61,7 @@ Player.prototype.update = function(dt) {
         this.reset()
         this.score++
         $('.badge').text(this.score)
+        this.increaseDifficulity()
     }
 };
 
@@ -72,6 +90,12 @@ Player.prototype.handleInput = function(value){
     }
 }
 
+Player.prototype.increaseDifficulity = function(){
+    for (var i = 0 ; i< allEnemies.length ; i++){
+        allEnemies[i].speed = (Math.random()*(200 + (this.score * 20) - this.score * 20))+this.score * 20
+    }
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -87,7 +111,7 @@ var allEnemies = [Enemy1, Enemy2, Enemy3, Enemy4, Enemy5 ]
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -98,3 +122,24 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+$('.character').click(function(){
+    id = this.id
+    var img = document.getElementById(id)
+    console.log(img.src)
+    player.sprite = img.src
+})
+
+var images = [
+     "images/char-boy.png",
+    "images/char-cat-girl.png",
+    "images/char-princess-girl.png",
+    "images/char-horn-girl.png",
+    "images/char-pink-girl.png"
+]
+Resources.load(images);
+
+$(document).ready(function(){
+    $("reload").click(function(){
+        location.reload(true);
+    });
+});
